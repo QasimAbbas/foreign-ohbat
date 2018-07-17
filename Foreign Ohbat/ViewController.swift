4b8c652f3161bb4943fca3a869d9a694b275c24c
Qasim Abbas <qasimabbas52@gmail.com>
2018-07-17 14:49:59 -0400

    Adding initial files

---
+++ Foreign Ohbat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foreign Ohbat
+//
+//  Created by Qasim Abbas on 7/17/18.
+//  Copyright © 2018 Qasim Abbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
